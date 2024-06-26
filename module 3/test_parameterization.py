b51cbcb8fbde4f5d5ed389638e6aeb2b840ce5e9
a.moinur <a.moinur@a1qa.com>
2024-06-26 21:18:30 +0600

    updated conftest and README.md files

--- module 3/test_parameterization.py
+++ module 3/test_parameterization.py
@@ -2,7 +2,6 @@ import pytest
 import json
 import time
 import math
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
